29296c4295aac097e9c12f20acf332323cab01ca
Youyi Laan <uyilaan@gmail.com>
2019-05-21 22:31:46 +0200

    added comments and cleaned up the code

--- test_code/led_matrix.c
+++ test_code/led_matrix.c
@@ -10,86 +10,121 @@
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 
+
+/**
+ * DEFINES
+ */
 #define FILEPATH "/dev/fb1"
 #define NUM_WORDS 64
 #define FILESIZE (NUM_WORDS * sizeof(uint16_t))
 
-#define RGB565_RED 0xFFE0
+/**
+ * Color is defined as RGB565
+ */
+#define COLOR 0xFFE0
 
-void delay(int);
+/**
+ * init custom sleep function
+ */
+void custom_sleep(int);
 
 int main(void)
 {
+    /**
+     * initialise variables
+     * int i: iterator
+     * int fbfd: framebuffer file descriptor
+     * uint16_t *map: unsigned 16 bit int for map pointer
+     * uint16_t *p: pointer to the map for writing to the led matrix
+     * struct fb_fix_screeninfo fix_info: struct for framebuffer
+     */
     int i;
     int fbfd;
     uint16_t *map;
     uint16_t *p;
     struct fb_fix_screeninfo fix_info;
 
-    /* open the led frame buffer device */
+    /**
+     * open the led frame buffer device, if returned -1, then exit with error
+     */
     fbfd = open(FILEPATH, O_RDWR);
     if (fbfd == -1) {
         perror("Error (call to 'open')");
         exit(EXIT_FAILURE);
     }
 
-    /* read fixed screen info for the open device */
+    /**
+     * read fixed screen info for the open device, this is the fixed screen info of the RPI-sense hat
+    */
     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &fix_info) == -1) {
-        perror("Error (call to 'ioctl')");
+        printf("%s\n", "Error: call to 'ioctl'");
         close(fbfd);
         exit(EXIT_FAILURE);
     }
 
     /* now check the correct device has been found */
     if (strcmp(fix_info.id, "RPi-Sense FB") != 0) {
-        printf("%s\n", "Error: RPi-Sense FB not found");
+        printf("%s\n", "Error: RPI-SENSE FB NOT FOUND");
         close(fbfd);
         exit(EXIT_FAILURE);
     }
 
-    /* map the led frame buffer device into memory */
+    /**
+     * map the led frame buffer device into memory using mmap
+     */
     map =
 	mmap(NULL, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
     if (map == MAP_FAILED) {
         close(fbfd);
-        perror("Error mmapping the file");
+        printf("%s\n", "Error: something went wrong with mmapping the file...");
         exit(EXIT_FAILURE);
     }
 
-    /* set a pointer to the start of the memory area */
+    /**
+     *  set a pointer to the start of the memory area,
+     *  this is used for looping through all the leds on the matrix
+     */
     p = map;
-
-    /* clear the led matrix */
+    /**
+     * clear matrix by writing 0 directly to map
+     */
     memset(map, 0, FILESIZE);
 
-    /* light up everything in a row, going from 0 -> 63 */
+    /**
+     * light up everything in a row by writing the color 
+     * to each value within *map starting with *p, going from 0 -> 63 
+     */
     for (i = 0; i < NUM_WORDS; i++) {
-        *(p + i) = RGB565_RED;
-        delay(25);
+        *(p + i) = COLOR;
+        custom_sleep(25);
     }
 
-    /* flash white */
-    for (i = 0; i < 3; i++) {
-        delay(250);
+    /**
+     * flash white twice times after all the leds have been lit up
+     */
+    for (i = 0; i < 2; i++) {
+        custom_sleep(250);
         memset(map, 0xFF, FILESIZE);
-        delay(250);
+        custom_sleep(250);
         memset(map, 0, FILESIZE);
     }
-    delay(250);
+    custom_sleep(250);
 
-    /* clear the led matrix */
     memset(map, 0, FILESIZE);
 
-    /* un-map and close */
+    /**
+     * un-map and close the connection
+     * no need to close the connection twice if munmap returns -1 
+     */
     if (munmap(map, FILESIZE) == -1) {
-        perror("Error un-mmapping the file");
+        printf("%s\n", "Error: something wrong happened with un-mmapping the file");
     }
     close(fbfd);
 
     return 0;
 }
 
-void delay(int t)
+void custom_sleep(int t)
 {
     usleep(t * 1000);
 }
--- test_code/pressure.c
+++ test_code/pressure.c
@@ -5,102 +5,137 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-#define DEV_ID 0x5c
+/**
+ * defines addresses of the pressure sensor
+ */
+#define DEV_ID          0x5c
+#define WHO_AM_I        0x0F
+#define CTRL_REG1       0x20
+#define CTRL_REG2       0x21
+#define PRESS_OUT_XL    0x28
+#define PRESS_OUT_L     0x29
+#define PRESS_OUT_H     0x2A
+#define TEMP_OUT_L      0x2B
+#define TEMP_OUT_H      0x2C
+
+/**
+ * defines different modes of the  
+ */
+#define POWER_DOWN      0x00
+#define EN_ONE_SHOT     0x84
+#define MESSURE         0x01
+
+/**
+ * define device path
+ */
 #define DEV_PATH "/dev/i2c-1"
-#define WHO_AM_I 0x0F
-#define CTRL_REG1 0x20
-#define CTRL_REG2 0x21
-#define PRESS_OUT_XL 0x28
-#define PRESS_OUT_L 0x29
-#define PRESS_OUT_H 0x2A
-#define TEMP_OUT_L 0x2B
-#define TEMP_OUT_H 0x2C
 
 void delay(int);
+void get_pressure_temp(void);
 
-int main(void)
+int main()
+{
+    while(1){
+        get_pressure_temp();
+        custom_sleep(1000);
+    }
+    return 0;
+}
+
+void get_pressure_temp(void)
 {
     int fd = 0;
     uint8_t temp_out_l = 0, temp_out_h = 0;
     int16_t temp_out = 0;
     double t_c = 0.0;
 
-    uint8_t press_out_xl = 0;
-    uint8_t press_out_l = 0;
-    uint8_t press_out_h = 0;
+    uint8_t press_out_xl    = 0;
+    uint8_t press_out_l     = 0;
+    uint8_t press_out_h     = 0;
 
     int32_t press_out = 0;
     double pressure = 0.0;
 
     uint8_t status = 0;
 
-    /* open i2c comms */
+    /**
+     * open i2c communication
+     */
     if ((fd = open(DEV_PATH, O_RDWR)) < 0) {
-	perror("Unable to open i2c device");
-	exit(1);
+        printf("%s\n", "Unable to open i2c device");
+        exit(1);
     }
 
-    /* configure i2c slave */
+    /**
+     * configure i2c slave 
+     */
     if (ioctl(fd, I2C_SLAVE, DEV_ID) < 0) {
-	perror("Unable to configure i2c slave device");
-	close(fd);
-	exit(1);
+        printf("%s\n", "Unable to configure i2c slave device");
+        close(fd);
+        exit(1);
     }
 
-    /* check we are who we should be */
+    /**
+     * check if WHO_AM_I value is correct and well 
+     */
     if (i2c_smbus_read_byte_data(fd, WHO_AM_I) != 0xBD) {
-	printf("%s\n", "who_am_i error");
-	close(fd);
-	exit(1);
+        printf("%s\n", "who_am_i error");
+        close(fd);
+        exit(1);
     }
 
     /* Power down the device (clean start) */
-    i2c_smbus_write_byte_data(fd, CTRL_REG1, 0x00);
+    i2c_smbus_write_byte_data(fd, CTRL_REG1, POWER_DOWN);
 
     /* Turn on the pressure sensor analog front end in single shot mode  */
-    i2c_smbus_write_byte_data(fd, CTRL_REG1, 0x84);
+    i2c_smbus_write_byte_data(fd, CTRL_REG1, EN_ONE_SHOT);
 
     /* Run one-shot measurement (temperature and pressure), the set bit will be reset by the
      * sensor itself after execution (self-clearing bit) */
-    i2c_smbus_write_byte_data(fd, CTRL_REG2, 0x01);
+    i2c_smbus_write_byte_data(fd, CTRL_REG2, MESSURE);
 
-    /* Wait until the measurement is complete */
+    /**
+     * Wait until the measurement is complete by checking the status
+     */
     do {
-	delay(25);		/* 25 milliseconds */
-	status = i2c_smbus_read_byte_data(fd, CTRL_REG2);
+        custom_sleep(25);		/* 25 milliseconds */
+        status = i2c_smbus_read_byte_data(fd, CTRL_REG2);
     }
     while (status != 0);
 
-    /* Read the temperature measurement (2 bytes to read) */
-    temp_out_l = i2c_smbus_read_byte_data(fd, TEMP_OUT_L);
-    temp_out_h = i2c_smbus_read_byte_data(fd, TEMP_OUT_H);
+    /**
+     *  Read the temperature and pressure measurements
+     * */
+    temp_out_l      = i2c_smbus_read_byte_data(fd, TEMP_OUT_L);
+    temp_out_h      = i2c_smbus_read_byte_data(fd, TEMP_OUT_H);
+    press_out_xl    = i2c_smbus_read_byte_data(fd, PRESS_OUT_XL);
+    press_out_l     = i2c_smbus_read_byte_data(fd, PRESS_OUT_L);
+    press_out_h     = i2c_smbus_read_byte_data(fd, PRESS_OUT_H);
 
-    /* Read the pressure measurement (3 bytes to read) */
-    press_out_xl = i2c_smbus_read_byte_data(fd, PRESS_OUT_XL);
-    press_out_l = i2c_smbus_read_byte_data(fd, PRESS_OUT_L);
-    press_out_h = i2c_smbus_read_byte_data(fd, PRESS_OUT_H);
+    /**
+     * Bitshift to get the correct value
+     */
+    temp_out        = temp_out_h << 8 | temp_out_l;
+    press_out       = press_out_h << 16 | press_out_l << 8 | press_out_xl;
 
-    /* make 16 and 24 bit values (using bit shift) */
-    temp_out = temp_out_h << 8 | temp_out_l;
-    press_out = press_out_h << 16 | press_out_l << 8 | press_out_xl;
-
-    /* calculate output values */
+    /**
+     *  calculate the output values
+     * */
     t_c = 42.5 + (temp_out / 480.0);
     pressure = press_out / 4096.0;
 
-    /*output */
     printf("Temp (from press) = %.2f°C\n", t_c);
     printf("Pressure = %.0f hPa\n", pressure);
 
     /* Power down the device */
-    i2c_smbus_write_byte_data(fd, CTRL_REG1, 0x00);
+    i2c_smbus_write_byte_data(fd, CTRL_REG1, POWER_DOWN);
 
     close(fd);
 
     return (0);
 }
 
-void delay(int t)
+void custom_sleep(int t)
 {
     usleep(t * 1000);
 }
